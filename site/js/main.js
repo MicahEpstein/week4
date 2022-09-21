a382b6ac809c0f34b7bff4bf9208fc8cc64ba864
MicahEpstein <mepstein@risd.edu>
2022-09-21 14:42:06 -0400

    adding data

--- site/js/main.js
+++ site/js/main.js
@@ -1,3 +1,7 @@
+import stops from '../data/stops.js';
+import universitycity from '../data/universitycity.js';
+
+
 let stopMap = L.map('stop-map').setView([39.95315006697334, -75.1973682197421], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
