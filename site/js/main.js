a6834e00164f8a89cc764d5f8386843d8027b6ca
MicahEpstein <mepstein@risd.edu>
2022-09-28 11:08:00 -0400

    actually listeners

--- site/js/main.js
+++ site/js/main.js
@@ -7,5 +7,13 @@ showStopsOnMap(stops, stopMap);
 
 let routeCheckboxes = document.querySelectorAll( '.route-checkbox');
 
+for (const cb of routeCheckboxes){
+    cb.addEventListener('change', (evt)=> {
+        console.log(`you clicked on the checkbox!`);
+        console.log(evt.target);
+    })
+}
+
 //make data ?global
 window.stops = stops;
+window.routeCheckboxes = routeCheckboxes;
